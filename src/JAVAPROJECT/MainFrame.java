e92d65af5511e0de8c545c13b2ff2ea1f22eb626
김설희 <s22042@gsm.hs.kr>
2022-10-17 20:48:23 +0900

    MainFrame

--- src/JAVAPROJECT/MainFrame.java
+++ src/JAVAPROJECT/MainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class MainFrame {
     public static void main(String args[]){
         Frame f = new Frame();
-        f.setTitle("쿠키");
+        f.setTitle("포춘쿠키");
         f.setBounds(300,300,300,300);
         f.setVisible(true);
 
