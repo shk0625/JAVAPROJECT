72b742c14d62a734fabf123640d75deec7603cf0
김설희 <s22042@gsm.hs.kr>
2022-11-06 02:34:42 +0900

    Update AddimgJP.java

--- src/JAVAPROJECT/AddimgJP.java
+++ src/JAVAPROJECT/AddimgJP.java
@@ -13,7 +13,7 @@ public class AddimgJP extends JFrame {
 
     JButton back = new JButton();
 
-    ImageIcon img3= new ImageIcon("포춘쿠키터지고눈뜸크기조정 ㅇ.png"); //jp3
+    ImageIcon img3= new ImageIcon("C:\\Users\\USER\\Desktop\\back test 1\\JAVAPROJECT\\src\\JAVAPROJECT\\image1\\포춘쿠키_안_터지고_눈감음_조정.png"); //jp3
     ImageIcon img4= new ImageIcon("포춘공룡.png"); //jp
 
 
@@ -23,14 +23,20 @@ public class AddimgJP extends JFrame {
       Dimension frameSize = getSize();
       Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+      jpb.setSize(500,300);
+      jpb.setBackground(Color.BLACK);
+
 
         lb= new JLabel("공룡",img4,SwingConstants.CENTER);
         lb3 = new JLabel("th",img3,SwingConstants.CENTER);
 
+
+        add(lb3); jp3.add(lb3);
+
         setLocation((windowSize.width-frameSize.width)/2,(windowSize.height-frameSize.height)/2);
         setVisible(true);
 
-      jpb.setBackground(Color.white);
+      jpb.setBackground(Color.blue);
 
       setLocation(400,200);
       setVisible(true);
