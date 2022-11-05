a26b7d2e9a26a6efa3cb5c2a0d8c8c4537809bf9
김설희 <s22042@gsm.hs.kr>
2022-11-06 07:25:05 +0900

    Update AddimgJP.java

--- src/JAVAPROJECT/AddimgJP.java
+++ src/JAVAPROJECT/AddimgJP.java
@@ -20,23 +20,17 @@ public class AddimgJP extends JFrame {
     public AddimgJP() {
       setTitle("포춘쿠키 출력");
       setSize(1000,700);
-      Dimension frameSize = getSize();
-      Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
-
       jpb.setSize(500,300);
-      jpb.setBackground(Color.pink);
+
 
         lb3 = new JLabel("fortune",ImageUtil.getImageNumber(터지고눈뜸),SwingConstants.CENTER);
-//        lb3.setSize(300,300);
+
 
         add(lb3); jp3.add(lb3);
         add(jp3);
-
-        setLocation((windowSize.width-frameSize.width)/2,(windowSize.height-frameSize.height)/2);
         setVisible(true);
 
-      jpb.setBackground(Color.gray);
-      jp3.setBackground(Color.green); //이게 출력 됨
+      jp3.setBackground(Color.white); //이게 출력 됨
 
       setLocation(400,200);
       setVisible(true);
@@ -45,16 +39,14 @@ public class AddimgJP extends JFrame {
         JButton back;
         back = new JButton("돌아가기");
         btnp.add(back,BorderLayout.SOUTH);
-        btnp.setSize(300,500);
-        /*back.setLocation(50,-150);*/
-        /*btnp.setLocation(100,-150);*/
+        btnp.setSize(500,500);
         btnp.setVisible(true);
         add(btnp);
 
         back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new MenuBar1("포춘쿠키Menubar");
+                new MenuBar1("fortune"); //MenuBar로 돌아감
                 setVisible(false);
             }
         });
