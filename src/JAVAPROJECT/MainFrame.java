5d549eb4b6b228e77adec66cca4a8920916050c6
김설희 <s22042@gsm.hs.kr>
2022-11-06 07:49:36 +0900

    lastest

--- src/JAVAPROJECT/MainFrame.java
+++ src/JAVAPROJECT/MainFrame.java
@@ -1,12 +1,16 @@
 package JAVAPROJECT;
 
+import JAVAPROJECT.db.FindList;
 import JAVAPROJECT.util.ImageUtil;
 
 import javax.swing.*;
+import java.sql.ResultSet;
 
 public class MainFrame {
     public static void main(String args[]){
         ImageUtil.setImage(); // 이미지 세팅
+
+        FindList.get();
 
         JFrame jf = new JFrame();
         jf.setTitle("포춘쿠키");
---
+++ src/JAVAPROJECT/db/LastWords.java
@@ -0,0 +1,10 @@
+package JAVAPROJECT.db;
+
+import javax.swing.*;
+
+public class LastWords extends JFrame {
+    JPanel jp = new JPanel();
+
+
+
+}
