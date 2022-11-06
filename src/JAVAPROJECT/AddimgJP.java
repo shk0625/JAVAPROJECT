393ee2c4f0e2430f2607e1d684be6672797c4538
김설희 <s22042@gsm.hs.kr>
2022-11-06 22:17:48 +0900

    Update AddimgJP.java

--- src/JAVAPROJECT/AddimgJP.java
+++ src/JAVAPROJECT/AddimgJP.java
@@ -1,11 +1,14 @@
 package JAVAPROJECT;
 
+import JAVAPROJECT.db.FindList;
 import JAVAPROJECT.util.ImageUtil;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.sql.ResultSet;
 
 import static JAVAPROJECT.util.ImagePrint.터지고눈뜸;
 
@@ -16,24 +19,43 @@ public class AddimgJP extends JFrame {
     JPanel jp3= new JPanel(); //터짐
 
     JLabel lb3 = new JLabel(); //터짐
+    JLabel lbwords = new JLabel();
 
     public AddimgJP() {
       setTitle("포춘쿠키 출력");
       setSize(1000,700);
       jpb.setSize(500,300);
-
-
-        lb3 = new JLabel("fortune",ImageUtil.getImageNumber(터지고눈뜸),SwingConstants.CENTER);
-
-
+        lb3 = new JLabel("fortune words",ImageUtil.getImageNumber(터지고눈뜸),SwingConstants.CENTER);
         add(lb3); jp3.add(lb3);
         add(jp3);
         setVisible(true);
-
       jp3.setBackground(Color.white); //이게 출력 됨
 
       setLocation(400,200);
       setVisible(true);
+
+      // txt 저장
+      String word = FindList.get();
+
+        try {
+            BufferedWriter output = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream("C:\\Users\\USER\\Desktop\\back test 1\\JAVAPROJECT\\src\\JAVAPROJECT\\static\\txt\\lastwords.txt", true),
+                            "UTF8"));
+            output.append(word + "\n");
+            output.close();
+        } catch (Exception e) {
+            System.out.println("e = " + e);
+        }
+
+        JTextField words = new JTextField(word);
+        add(lbwords); jpb.add(words); add(jpb);
+        lbwords.setSize(300,300);
+        lbwords.setLocation(150,SwingConstants.BOTTOM);
+
+        add(lb3); jp3.add(lb3);
+        add(jp3);
+        setVisible(true);
 
       //back button
         JButton back;
