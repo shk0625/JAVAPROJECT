61ba88582ec64e987d33a29137c22a51854933a8
김설희 <s22042@gsm.hs.kr>
2022-11-06 22:16:05 +0900

    Update LastWords.java

--- src/JAVAPROJECT/db/LastWords.java
+++ src/JAVAPROJECT/db/LastWords.java
@@ -1,10 +1,25 @@
 package JAVAPROJECT.db;
 
 import javax.swing.*;
+import java.io.*;
 
 public class LastWords extends JFrame {
     JPanel jp = new JPanel();
 
 
+
+    public static void main(String args[]){
+        File file = new File("C:\\Users\\USER\\Desktop\\myself\\1-3\\수행평가\\JAVA");
+
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+
+        }
+
+
+    }
 
 }
