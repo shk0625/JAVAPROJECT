7115c1d77f9660d3746c92aa9a037646cc88da29
김설희 <s22042@gsm.hs.kr>
2022-11-07 19:55:40 +0900

    Create Datasave.java

---
+++ src/JAVAPROJECT/db/Datasave.java
@@ -0,0 +1,24 @@
+package JAVAPROJECT.db;
+
+import java.util.Date;
+
+public class Datasave {
+    private String words;
+    private int day;
+
+    public String getWords() {
+        return words;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+}
