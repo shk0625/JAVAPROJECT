1e112167c8527f8a2b14eee648677e52e39410df
김설희 <s22042@gsm.hs.kr>
2022-11-09 19:39:25 +0900

    Update LastWords.java

--- src/JAVAPROJECT/db/LastWords.java
+++ src/JAVAPROJECT/db/LastWords.java
@@ -2,14 +2,17 @@ package JAVAPROJECT.db;
 
 import javax.swing.*;
 import java.io.*;
-
+//내가 뽑았떤 문구들
 public class LastWords extends JFrame {
     JPanel jp = new JPanel();
 
 
 
     public static void main(String args[]){
-        File file = new File("C:\\Users\\USER\\Desktop\\myself\\1-3\\수행평가\\JAVA");
+
+
+
+       /* File file = new File("C:\\Users\\USER\\Desktop\\myself\\1-3\\수행평가\\JAVA");
 
         try{
             FileOutputStream fileOutputStream = new FileOutputStream(file, true);
@@ -17,7 +20,7 @@ public class LastWords extends JFrame {
         }catch (FileNotFoundException e){
             e.printStackTrace();
 
-        }
+        }*/
 
 
     }
