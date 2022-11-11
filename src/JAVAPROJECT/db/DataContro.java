c6428d5686c4ea228c358bd3cf2ee34e6a468504
김설희 <s22042@gsm.hs.kr>
2022-11-11 12:10:49 +0900

    Update DataContro.java

--- src/JAVAPROJECT/db/DataContro.java
+++ src/JAVAPROJECT/db/DataContro.java
@@ -42,49 +42,50 @@ public class DataContro extends JFrame {
 
 
         //31개의 데이터들을 choice에 넣는 거
-        /*ArrayList<Datasave> list = new ArrayList<Datasave>();*/
-        /*for (Datasave printt : list) {
-            words.addItem(printt.getDay() + "번째 " + printt.getWords());
-        }*/
-        Choice words = new Choice();
-        words.addItem("무엇인가 좋은 결실을 이룰 것 같은 날입니다.");
-        words.addItem("모든 일이 생각대로 잘 풀리는 날입니다.");
-        words.addItem("오늘보다 내일을 기약하는 것은 어떨까요?");
-        words.addItem("길게 보는 안목과 혜안이 필요합니다..");
-        words.addItem("오늘 할 일은 오늘 하는 게 어떨까요?");
+        /*Choice words = new Choice();*/
+        TextArea words = new TextArea(40,40);
 
-        words.addItem("모든 사람들로부터 사랑받지 않아도 됩니다.");
-        words.addItem("미래를 만드는 건 현재입니다.");
-        words.addItem("운명의 별은 마음속에 있습니다.");
-        words.addItem("미래는 예측하는 게 아니라 만들어 가는 겁니다.");
-        words.addItem("피할 수 없으면 즐기는 게 어떨까요?");
 
-        words.addItem("고난의 시기에 동요하지않는 건 어떨까요?");
-        words.addItem("작은 기회를 노려보는 건 어떨까요?");
-        words.addItem("용기있게 산다면 이룰 것 같은 날입니다.");
-        words.addItem("애정 운이 최고조에 달하는 날입니다.");
-        words.addItem("선택과 집중");
+        words.setText("무엇인가 좋은 결실을 이룰 것 같은 날입니다.");
+        words.setText("모든 일이 생각대로 잘 풀리는 날입니다.");
+        words.setText("오늘보다 내일을 기약하는 것은 어떨까요?");
+        words.setText("길게 보는 안목과 혜안이 필요합니다..");
+        words.setText("오늘 할 일은 오늘 하는 게 어떨까요?");
 
-        words.addItem("집중하면 성공할 것입니다.");
-        words.addItem("운은 계획에서 비롯됩니다.");
-        words.addItem("완벽함을 위한 것이 아닌 탁월함을 위해 힘 써보세요.");
-        words.addItem("반성하는 삶을 살아보는 건 어떨까요?");
-        words.addItem("자책하지 마세요 누구나 실수는 합니다.");
+        words.setText("모든 사람들로부터 사랑받지 않아도 됩니다.");
+        words.setText("미래를 만드는 건 현재입니다.");
+        words.setText("운명의 별은 마음속에 있습니다.");
+        words.setText("미래는 예측하는 게 아니라 만들어 가는 겁니다.");
+        words.setText("피할 수 없으면 즐기는 게 어떨까요?");
 
-        words.addItem("후회없는 하루를 보내보세요");
-        words.addItem("무언가를 시작하기 전에 충분한 정리가 필요합니다.");
-        words.addItem("자기 자신을 사랑한다면 사랑받을 수 있습니다.");
-        words.addItem("질투와 시기를 자신을 발전시키는 원동력으로 바꿔보는 것은 어떨까요?");
-        words.addItem("당신은 세상의 소금같은 존재입니다.");
+        words.setText("고난의 시기에 동요하지않는 건 어떨까요?");
+        words.setText("작은 기회를 노려보는 건 어떨까요?");
+        words.setText("용기있게 산다면 이룰 것 같은 날입니다.");
+        words.setText("애정 운이 최고조에 달하는 날입니다.");
+        words.setText("선택과 집중");
 
-        words.addItem("하루하루를 의미있게 보내보세요.");
-        words.addItem("소중한 사람을 위해 무언가를 준비해보는 것은 어떨까요?");
-        words.addItem("하루를 의미있게 보내는 방법을 연구해보아요.");
-        words.addItem("고민보다 Go!");
-        words.addItem("한계는 또 다른 한계를 찾는 거예요.");
+        words.setText("집중하면 성공할 것입니다.");
+        words.setText("운은 계획에서 비롯됩니다.");
+        words.setText("완벽함을 위한 것이 아닌 탁월함을 위해 힘 써보세요.");
+        words.setText("반성하는 삶을 살아보는 건 어떨까요?");
+        words.setText("자책하지 마세요 누구나 실수는 합니다.");
 
-        words.addItem("자신이 1순위라는 것을 잊지 말아요.");
+        words.setText("후회없는 하루를 보내보세요");
+        words.setText("무언가를 시작하기 전에 충분한 정리가 필요합니다.");
+        words.setText("자기 자신을 사랑한다면 사랑받을 수 있습니다.");
+        words.setText("질투와 시기를 자신을 발전시키는 원동력으로 바꿔보는 것은 어떨까요?");
+        words.setText("당신은 세상의 소금같은 존재입니다.");
 
+        words.setText("하루하루를 의미있게 보내보세요.");
+        words.setText("소중한 사람을 위해 무언가를 준비해보는 것은 어떨까요?");
+        words.setText("하루를 의미있게 보내는 방법을 연구해보아요.");
+        words.setText("고민보다 Go!");
+        words.setText("한계는 또 다른 한계를 찾는 거예요.");
+
+        words.setText("자신이 1순위라는 것을 잊지 말아요.");
+        //textarea 겹쳐지무ㅜ.. 다 추가가 되어야하는데...
+
+        words.setFont(new Font("궁서체",Font.BOLD,20));
 
         add(words);
         jp.add(words);
@@ -92,6 +93,8 @@ public class DataContro extends JFrame {
         jp.setVisible(true);
         jp.setLocation(400, 200);
         setVisible(true);
+
+        /*setSize(1000, 700);*/
 
     }
         public static void main (String args[] ){
@@ -127,6 +130,7 @@ public class DataContro extends JFrame {
             }
             for (Datasave model : list) {
                 System.out.println(model.getDay() + "번째 문구 : " + model.getWords());
+
             }
 
 
