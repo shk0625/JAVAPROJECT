9f8e4d0b16b827872d9a8c060ba3c93f2cdec9ce
김설희 <s22042@gsm.hs.kr>
2022-11-16 11:26:56 +0900

    Update LastWords.java

--- src/JAVAPROJECT/db/LastWords.java
+++ src/JAVAPROJECT/db/LastWords.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.plaf.basic.BasicArrowButton;
 import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
+
 //내가 뽑았던 문구들
 public class LastWords extends JFrame {
 
@@ -14,22 +16,33 @@ public class LastWords extends JFrame {
 
         back.add(back,BorderLayout.SOUTH);
         jp.add(back);
+        back.setLocation(300,100);
 
+   /*     lastwords.setBackground(Color.WHITE);
+        jp.setBackground(Color.white); 왜 이 3개 다 안 됨?
+        setBackground(Color.white);*/
+        setSize(1000,700);
+        setLocation(400,200);
+        setVisible(true);
 
-    }
-    public static void main(String args[]){
+        //txt파일을 바탕화면에 저장
+        File file = new File("C:\\Users\\USER\\Desktop\\myself\\1-3\\수행평가\\JAVA"); //LastWords로 된 파일 있음
+        String getword = FindList.get();
 
-
-
-       /* File file = new File("C:\\Users\\USER\\Desktop\\myself\\1-3\\수행평가\\JAVA");
-
-        try{
+        try {
             FileOutputStream fileOutputStream = new FileOutputStream(file, true);
 
         }catch (FileNotFoundException e){
             e.printStackTrace();
+        }
+
+     /*   ArrayList<LastWords>
+        for(int...getword){
 
         }*/
+
+    }
+    public static void main(String args[]){
 
 
     }
