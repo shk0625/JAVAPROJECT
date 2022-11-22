c050cf78b6a63bebe1d6449461b6cedac3057d7b
김설희 <s22042@gsm.hs.kr>
2022-11-22 14:06:31 +0900

    Update MenuBar1.java

--- src/JAVAPROJECT/MenuBar1.java
+++ src/JAVAPROJECT/MenuBar1.java
@@ -33,9 +33,7 @@ public class MenuBar1 extends JFrame {
 
         Menu show = new Menu("보기");
         MenuItem show_last = new MenuItem("지난 날의 문구들...");
-        /*MenuItem show_draft = new MenuItem("초안");*/
         show.add(show_last);
-        /*show.add(show_draft);*/
 
         mb.add(file); mb.add(show);
 
